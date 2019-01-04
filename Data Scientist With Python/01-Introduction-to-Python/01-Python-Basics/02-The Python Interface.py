e83d7b6d76685e92f1287c4a8d26891e1c4737e6
Pappu Kumar <pappukr4444@gmail.com>
2019-01-04 15:38:16 +0530

    Update 02-The Python Interface.py

--- Data Scientist With Python/01-Introduction-to-Python/01-Python-Basics/02-The Python Interface.py
+++ Data Scientist With Python/01-Introduction-to-Python/01-Python-Basics/02-The Python Interface.py
@@ -1,6 +1,6 @@
 '''
 The Python Interface - 100 XP
-
+============================================================
 In the Python script on the right, you can type Python code to solve the exercises. If you hit Run Code or Submit Answer, your python script (script.py) is executed and the output is shown in the IPython Shell. Submit Answer checks whether your submission is correct and gives you feedback.
 
 You can hit Run Code and Submit Answer as often as you want. If you're stuck, you can click Get Hint, and ultimately Get Solution.
@@ -9,14 +9,17 @@ You can also use the IPython Shell interactively by simply typing commands and h
 
 
 Instructions
-
+============================================================
 Experiment in the IPython Shell; type 5 / 8, for example.
+
 Add another line of code to the Python script: print(7 + 10).
+
 Hit Submit Answer to execute the Python script and receive feedback.
 
+
+Code
+============================================================
 '''
-
-
 # Example, do not modify!
 print(5 / 8)
 
