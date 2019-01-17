cfd5f2dce882780c57e8d9121b030c16d422b06b
Pappu Kumar <pappukr4444@gmail.com>
2019-01-17 16:21:09 +0530

    Add files via upload

---
+++ Data Scientist With Python/01-Introduction-to-Python/02-Python-Lists/04-Select-the-valid-list.py
@@ -0,0 +1,28 @@
+'''
+Select the valid list - 50 XP
+==============================================================
+
+A list can contain any Python type. But a list itself is also a Python type. That means that a list can also contain a list! Python is getting funkier by the minute, but fear not, just remember the list syntax:
+
+my_list = [el1, el2, el3]
+
+Can you tell which ones of the following lines of Python code are valid ways to build a list?
+
+A. [1, 3, 4, 2] B. [[1, 2, 3], [4, 5, 7]] C. [1 + 2, "a" * 5, 3]
+
+
+Instructions
+==============================================================
+
+Possible Answers
+
+  	1. A, B and C
+    2. B
+    3. B and C
+    4. C
+    
+Answers - 1
+
+Code
+==============================================================
+'''
